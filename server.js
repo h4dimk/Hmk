25b41632140d462fa42167c7615d2fea486a0f3d
h4dimk <hadimk04@gmail.com>
2023-10-27 18:45:24 +0530

    updated

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "Public")));
 app.use('/uploads', express.static('uploads'));
 app.use(nocache())
 app.use(morgan("dev"));   
